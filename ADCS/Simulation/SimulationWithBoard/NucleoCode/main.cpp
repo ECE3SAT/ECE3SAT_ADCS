fe8853b0b9f94702f33d15c68ce7cd409fe23fe2
ECE3SAT <ece3sat.@gmail.com>
2017-04-06 15:33:29 +0200

    Update main.cpp

--- ADCS/Simulation/SimulationWithBoard/NucleoCode/main.cpp
+++ ADCS/Simulation/SimulationWithBoard/NucleoCode/main.cpp
@@ -1,13 +1,13 @@
 /**
  ******************************************************************************
  * @file    main.cpp
- * @author  AST / EST
+ * @author  Gael Guegan
  * @version V0.0.1
- * @date    10.03.17
- * @brief   Simple Example application for using the X_NUCLEO_IKS01A1
+ * @date    06-April-2017
+ * @brief   Simple Example application for using the X_NUCLEO_IKS01A1 
  *          MEMS Inertial & Environmental Sensor Nucleo expansion board.
  ******************************************************************************
-**/
+**/ 
 
 /* Includes */
 #include "mbed.h"
@@ -21,28 +21,27 @@ static GyroSensor *gyroscope = mems_expansion_board->GetGyroscope();
 static MotionSensor *accelerometer = mems_expansion_board->GetAccelerometer();
 static MagneticSensor *magnetometer = mems_expansion_board->magnetometer;
 
+Serial pc(SERIAL_TX, SERIAL_RX);
 
 DigitalOut myled(LED2);
 
 /* Simple main function */
 int main() {
-
+  pc.baud(300000);
   int32_t axes[3];
-
+  
   while(1) {
-
+      
     myled=!myled;
 
     magnetometer->Get_M_Axes(axes);
-    printf("m/%6ld/%6ld/%6ld/  \r\n", axes[0], axes[1], axes[2]);    //"LIS3MDL [mag/mgauss]
+    pc.printf("m/%6ld/%6ld/%6ld/  \r\n", axes[0], axes[1], axes[2]);    //LIS3MDL [mag/mgauss]
 
     accelerometer->Get_X_Axes(axes);
-    printf("a/%6ld/%6ld/%6ld/  \r\n", axes[0], axes[1], axes[2]);    //LSM6DS0 [acc/mg]
+    pc.printf("a/%6ld/%6ld/%6ld/  \r\n", axes[0], axes[1], axes[2]);    //LSM6DS0 [acc/mg]
 
     gyroscope->Get_G_Axes(axes);
-    printf("g/%6ld/%6ld/%6ld/  \r\n", axes[0], axes[1], axes[2]);    //LSM6DS0 [gyro/mdps]
-
-    //wait(0.2);
-
+    pc.printf("g/%6ld/%6ld/%6ld/  \r\n", axes[0], axes[1], axes[2]);    //LSM6DS0 [gyro/mdps]
+    
   }
 }
